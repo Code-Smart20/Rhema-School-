0366362f251555d69271895bdefbe1b835697a76
nwachukwu prince <bldrnwagod@gmail.com>
2023-06-22 07:06:07 -0700

    popular Courses section and Faqs markup and Css

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-// change nav bar on scroll
+// change nav bar on scroll of the window object
+
+window.addEventListener("scroll",()=>{
+    document.querySelector("nav").classList.toggle("window-scroll",window.scrollY > 0)
+})
